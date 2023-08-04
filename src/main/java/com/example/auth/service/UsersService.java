f7a562096c4047c1fd3e6bbde3c52e7b06e27288
vatanhuseynov <vatanhuseynov1@gmail.com>
2023-08-04 23:21:17 +0400

    refactor

--- src/main/java/com/example/auth/service/UsersService.java
+++ src/main/java/com/example/auth/service/UsersService.java
@@ -22,26 +22,26 @@ public class UsersService {
             return Optional.empty();
         }
     }
-    public Users login(Users user){
-        Optional<Users> existUser = userRepo.findUsersByUserName(user.getUserName());
-        if(existUser.isPresent() && existUser.get().getPassword().equals(user.getPassword())){
-             existUser.get().setToken(UUID.randomUUID().toString());
-             userRepo.save(existUser.get());
-            return existUser.get();
-    }
-            return null;
-        }
 
-    public ResponseUserDto verifyUser(String token){
-       Optional<Users> existUser =  userRepo.findUsersByToken(token);
-       if(existUser.isPresent()){
-           ResponseUserDto userDto = ResponseUserDto.builder()
-                   .userName(existUser.get().getUserName())
-                   .name(existUser.get().getName())
-                   .surName(existUser.get().getSurName()).build();
-      return userDto;
-       }
-       return null;
+    public Users login(Users user) {
+        Optional<Users> existUser = userRepo.findUsersByUserName(user.getUserName());
+        if (existUser.isPresent() && existUser.get().getPassword().equals(user.getPassword())) {
+            existUser.get().setToken(UUID.randomUUID().toString());
+            userRepo.save(existUser.get());
+            return existUser.get();
+        }
+        return null;
+    }
+
+    public ResponseUserDto verifyUser(String token) {
+        Optional<Users> existUser = userRepo.findUsersByToken(token);
+        if (existUser.isPresent()) {
+            return ResponseUserDto.builder()
+                    .userName(existUser.get().getUserName())
+                    .name(existUser.get().getName())
+                    .surName(existUser.get().getSurName()).build();
+        }
+        return null;
     }
 
 }
